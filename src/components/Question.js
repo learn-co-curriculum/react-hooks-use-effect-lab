94e95e0c7e81cc824fa87677d907200e939e8163
ihollander <ianholla@gmail.com>
2020-12-14 12:02:00 -0500

    Added solution

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+
+    return function () {
+      clearInterval(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
 
   const { id, prompt, answers, correctIndex } = question;
 
